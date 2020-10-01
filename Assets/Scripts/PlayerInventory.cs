2f06f44579f86facc93c9410fbe47ee403e5657d
john morris <johncmorris@gmail.com>
2020-10-01 09:26:59 -0700

    end of day 1

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -5,12 +5,37 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Inventory", menuName = "ScriptableObject/Inventory")]
 public class PlayerInventory : ScriptableObject
 {
-    public bool hasYellowKey;
+    public int YellowKeyCount = 0;
+
+    public bool hasYellowKey {
+        get
+        {
+            return useYellowKey();
+        }
+        set
+        {
+            if (value) YellowKeyCount += 1;
+            else YellowKeyCount = 0;
+        }
+    }
     public bool hasDoubleJump;
 
     private void Awake()
     {
         Debug.Log("Invetory Added");
 
+    }
+
+    private bool useYellowKey()
+    {
+        if(YellowKeyCount > 0)
+        {
+            YellowKeyCount -= 1;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
