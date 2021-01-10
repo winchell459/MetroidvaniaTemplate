68562328fb9701022658d6e7b53ab12a91ea7ec1
winchell459 <johncmorris@gmail.com>
2021-01-10 11:00:24 -0800

    end of day 13

---
+++ Assets/Scripts/Testing/SquareColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareColor : MonoBehaviour
+{
+    public enum Colors
+    {
+        yellow,
+        blue,
+        red,
+        green,
+        magenta
+    }
+    public Colors MyColor;
+}
---
+++ Assets/Scripts/Testing/Testing2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing2 : MonoBehaviour
+{
+    int[] myArray = { 3, 7, 1, 5, 9 }; // -> {1,3,5,7,9}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SquareColor[] squares = FindObjectsOfType<SquareColor>();
+        for (int j = 1; j < squares.Length; j += 1)
+        {
+            for (int i = 1; i < squares.Length; i += 1)
+            {
+                if (squares[i - 1].transform.position.y > squares[i].transform.position.y)
+                {
+                    SquareColor temp = squares[i];
+                    squares[i] = squares[i - 1];
+                    squares[i - 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < squares.Length; i += 1)
+        {
+            squares[i].transform.position = new Vector3(0, 1.25f * i - 3, 0);
+        }
+
+
+
+
+
+        for(int j = 0; j < myArray.Length; j += 1)
+        {
+            for(int i = 1; i <myArray.Length; i += 1)
+            {
+                if(myArray[i-1] > myArray[i])
+                {
+                    int temp = myArray[i];
+                    myArray[i] = myArray[i - 1];
+                    myArray[i - 1] = temp;
+                }
+            }
+        }
+
+        
+        foreach(int num in myArray)
+        {
+            print(num);
+        }
+    }
+    
+}
